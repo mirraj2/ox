8d4e880eac5217181c8ca725a82d8c610f16342a
Jason Mirra <mirraj2@gmail.com>
2016-08-02 16:15:46 -0700

    fix

--- src/ox/util/Utils.java
+++ src/ox/util/Utils.java
@@ -153,6 +153,9 @@ public class Utils {
   }
 
   public static String formatPhone(String phoneNumber) {
+    if (phoneNumber.length() != 10) {
+      return phoneNumber;
+    }
     return "(" + phoneNumber.substring(0, 3) + ") " + phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6, 10);
   }
 
