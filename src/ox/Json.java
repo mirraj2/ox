a19456486b53c0de2cdc0b186072e4df9e4da1a2
Jason Mirra <mirraj2@gmail.com>
2016-11-30 10:43:27 -0800

    bug fix

--- src/ox/Json.java
+++ src/ox/Json.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
@@ -108,7 +109,7 @@ public class Json implements Iterable<String> {
 
   public boolean has(String key) {
     JsonElement e = getElement(key);
-    if (e == null) {
+    if (e == null || e instanceof JsonNull) {
       return false;
     }
     if (e.isJsonPrimitive()) {
