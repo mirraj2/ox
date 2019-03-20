56aa2b77cb9377538d06413cbd6c201d5485fba7
Jason <mirraj2@gmail.com>
2019-03-19 22:36:37 -0500

    minor

--- src/ox/HttpRequest.java
+++ src/ox/HttpRequest.java
@@ -87,72 +87,38 @@ import com.google.common.io.BaseEncoding;
 public class HttpRequest {
 
   public static final String CHARSET_UTF8 = "UTF-8";
-
   public static final String CONTENT_TYPE_FORM = "application/x-www-form-urlencoded";
-
   public static final String CONTENT_TYPE_JSON = "application/json";
-
   public static final String ENCODING_GZIP = "gzip";
-
   public static final String HEADER_ACCEPT = "Accept";
-
   public static final String HEADER_ACCEPT_CHARSET = "Accept-Charset";
-
   public static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
-
   public static final String HEADER_AUTHORIZATION = "Authorization";
-
   public static final String HEADER_CACHE_CONTROL = "Cache-Control";
-
   public static final String HEADER_CONTENT_ENCODING = "Content-Encoding";
-
   public static final String HEADER_CONTENT_LENGTH = "Content-Length";
-
   public static final String HEADER_CONTENT_TYPE = "Content-Type";
-
   public static final String HEADER_DATE = "Date";
-
   public static final String HEADER_ETAG = "ETag";
-
   public static final String HEADER_EXPIRES = "Expires";
-
   public static final String HEADER_IF_NONE_MATCH = "If-None-Match";
-
   public static final String HEADER_LAST_MODIFIED = "Last-Modified";
-
   public static final String HEADER_LOCATION = "Location";
-
   public static final String HEADER_PROXY_AUTHORIZATION = "Proxy-Authorization";
-
   public static final String HEADER_REFERER = "Referer";
-
   public static final String HEADER_SERVER = "Server";
-
   public static final String HEADER_USER_AGENT = "User-Agent";
-
   public static final String METHOD_DELETE = "DELETE";
-
   public static final String METHOD_GET = "GET";
-
   public static final String METHOD_HEAD = "HEAD";
-
   public static final String METHOD_OPTIONS = "OPTIONS";
-
   public static final String METHOD_POST = "POST";
-
   public static final String METHOD_PUT = "PUT";
-
   public static final String METHOD_TRACE = "TRACE";
-
   public static final String PARAM_CHARSET = "charset";
-
   private static final String BOUNDARY = "00content0boundary00";
-
-  private static final String CONTENT_TYPE_MULTIPART = "multipart/form-data; boundary="
-      + BOUNDARY;
-
+  private static final String CONTENT_TYPE_MULTIPART = "multipart/form-data; boundary=" + BOUNDARY;
   private static final String CRLF = "\r\n";
-
   private static final String[] EMPTY_STRINGS = new String[0];
 
   private static SSLSocketFactory TRUSTED_FACTORY;
@@ -1589,8 +1555,8 @@ public class HttpRequest {
     return this;
   }
 
-  public URL url() {
-    return getConnection().getURL();
+  public String url() {
+    return getConnection().getURL().toString();
   }
 
   public String method() {
