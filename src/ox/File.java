103299de85807c4bbd137d4b45c3ecc29d40947a
Jason Mirra <mirraj2@gmail.com>
2020-07-10 10:00:20 -0500

    File.streamLines

--- src/ox/File.java
+++ src/ox/File.java
@@ -3,9 +3,14 @@ package ox;
 import static com.google.common.base.Preconditions.checkState;
 import static ox.util.Functions.map;
 import static ox.util.Utils.getExtension;
+import static ox.util.Utils.propagate;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.util.List;
+
+import com.google.common.base.Predicate;
 
 public class File {
 
@@ -102,12 +107,35 @@ public class File {
     OS.open(file);
   }
 
+  public void streamLines(Predicate<String> callback) {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(file));
+      while (true) {
+        String line = br.readLine();
+        if (line == null || !callback.apply(line)) {
+          break;
+        }
+      }
+      IO.close(br);
+    } catch (Exception e) {
+      throw propagate(e);
+    }
+  }
+
   public static File desktop() {
     return new File(OS.getDesktop());
   }
 
   public static File desktop(String child) {
-    return new File(OS.getDesktop()).child(child);
+    return desktop().child(child);
+  }
+
+  public static File downloads() {
+    return new File(OS.getDownloadsFolder());
+  }
+
+  public static File downloads(String child) {
+    return downloads().child(child);
   }
 
   public static File temp(String child) {
