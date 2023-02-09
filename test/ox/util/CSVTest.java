5b88f116ce782b40bd025f17df8c316aba120d9d
Jake Mirra <jpluto909@gmail.com>
2023-02-09 13:53:22 -0600

    Unit test.

--- test/ox/util/CSVTest.java
+++ test/ox/util/CSVTest.java
@@ -1,12 +1,15 @@
 package ox.util;
 
 import static com.google.common.base.Preconditions.checkState;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.Test;
 
+import ox.IO;
 import ox.x.XList;
 
 public class CSVTest {
@@ -24,6 +27,24 @@ public class CSVTest {
     XList<String> output = reader.nextLine();
 
     checkState(input.equals(output), input + " vs " + output);
+  }
+
+  /**
+   * Whitespace characters are trimmed from header Strings for use as keys.
+   */
+  @Test
+  public void nospaceCharTest() {
+    // Here there is a leading no-break whitespace character in the header.
+    String content = "\uFEFF" + "foo, bar\n" + "42, 69";
+    CSVReader reader = CSVReader.from(IO.from(content).asStream());
+    AtomicBoolean found42 = new AtomicBoolean(false);
+    reader.forEachRow(row -> {
+      if (row.get("foo").equals("42")) {
+        found42.set(true);
+      }
+    });
+
+    assertTrue(found42.get());
   }
 
 }
